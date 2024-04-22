f4f56b8a5bed73c54dd22a47f9027b988d9f6d64
Kalyan <katragaddakalyan2@gmail.com>
2024-04-22 12:53:14 -0400

    Added new sample data

--- src/app/sampleData.ts
+++ src/app/sampleData.ts
@@ -15,7 +15,19 @@ export const  contactList: Array<ContactObject> = [
     {id: 12, firstName: 'Lorem', lastName: 'Ipsum', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
     {id: 13, firstName: 'Kate', lastName: 'Doe', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
     {id: 14, firstName: 'Julie', lastName: 'Kate', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
-    {id: 15, firstName: 'Patrick', lastName: 'Williams', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'}
+    {id: 15, firstName: 'Patrick', lastName: 'Williams', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 16, firstName: 'Dave', lastName: 'Jack', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 17, firstName: 'Elize', lastName: 'Williams', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 18, firstName: 'Faiser', lastName: 'Kate', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 19, firstName: 'Gina', lastName: 'Williams', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 20, firstName: 'Ingle', lastName: 'Watt', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 21, firstName: 'Junior', lastName: 'Watt', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 22, firstName: 'Kaiser', lastName: 'Watt', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 23, firstName: 'Lane', lastName: 'Watt', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 24, firstName: 'Otter', lastName: 'Spring', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'},
+    {id: 25, firstName: 'Reeger', lastName: 'Watt', email: 'reena.bushanda@gmail.com', phone: '9876543210', address:'132, My Street, Kingston, New York 12401', note: 'Adam\'s NY address'}
+
+
 
 
   ];
